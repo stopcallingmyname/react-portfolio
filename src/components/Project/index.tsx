f6ea45843dbfecd8b0016ac577fac82c5b592dff
stopcallingmyname <x666rxse@gmail.com>
2024-02-20 02:14:33 +0300

    refactoring preview

--- src/components/Project/index.tsx
+++ src/components/Project/index.tsx
@@ -1,40 +1,25 @@
 import styles from './styles.module.scss';
-import {APIResponse, LinkPreview} from '@dhaiwat10/react-link-preview';
-import {parse} from 'html-metadata-parser';
+
 type ProjectProps = {
   gitUrl?: string;
   publicUrl?: string;
 };
-const customFetcher = async (url: string): Promise<APIResponse> => {
-  const result = await parse(url, {
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-    },
-  });
 
-  return {
-    title: result.og.title ?? null,
-    description: result.og.description ?? null,
-    image: result.og.image ?? null,
-    siteName: result.og.site_name ?? null,
-    hostname: result.og.site_name ?? null,
-  };
-};
 function Project({gitUrl, publicUrl}: ProjectProps) {
   return (
     <>
       <section className={styles['wrapper']}>
+        <img
+          src="https://cdn.dribbble.com/users/2547736/screenshots/16309212/media/dfc6571df7ce9adadc6b4d2987558e53.png"
+          className={styles.preview}
+        ></img>
+        <div className={styles['title-wrapper']}>
+          <p>Weather Task</p>
+        </div>
         <div className={styles.links}>
           {gitUrl && <a href={gitUrl}>Source</a>}
           {publicUrl && <a href={publicUrl}>Live Demo</a>}
           {!gitUrl && !publicUrl && <p>Soon..</p>}
-          <LinkPreview
-            url="https://github.com/dhaiwat10/react-link-preview/blob/HEAD/demo.gif"
-            width={'400px'}
-            fetcher={customFetcher}
-          />
-          {/* <a href={gitUrl}>Source</a>
-          <a href={publicUrl}>Live Demo</a> */}
         </div>
       </section>
     </>
